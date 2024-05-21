ed416a2bd3c73e406a2348ee0735e89bafc7dfe9
Leandro <leandro.almeida@uvvnet.com.br>
2024-05-21 13:37:40 -0300

    add person

---
+++ RestWithASPNET.API/models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithASPNET.API.models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstLast { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+}
